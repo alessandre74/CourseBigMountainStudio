e5cf70c3bdc84619be748f5347a630b874be99bd
Alessandre Livramento <aleliv74@gmail.com>
2023-05-31 14:22:39 -0300

    Course Big Mountain Studio

--- 1 - Bluprint/Bluprint/ContentView.swift
+++ 1 - Bluprint/Bluprint/ContentView.swift
@@ -8,17 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        ViewOne()
-//        ViewTwo()
+  var body: some View {
+//        ViewOne()
+    ViewTwo()
 //        ViewThree()
 //        ViewFour()
 //        ViewFive()
-    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
